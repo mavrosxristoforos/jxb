4e6f22471903dc638fed40b856c4d5e74f9ad408
Christopher Mavros <mavrosxristoforos@gmail.com>
2019-11-26 20:55:45 +0200

    Copy, Delete, Mkdir, Rename commands

--- README.md
+++ README.md
@@ -15,17 +15,24 @@ The path of the file from which to retrieve the release version.
 
 ## Outputs
 
-The package version.
+### `output-version`
+
+The package version to use for the release.
 
 ## Example usage
 
     uses: mavrosxristoforos/jxb@master
       with:
         build-file: 'build.jxb'
-        ignore-inc-version: true
+
+## Console usage
+
+    node index.js -f path/to/buildfile
   
 ## Available Syntax for Build files
-  - `DELETE:file_to_delete`
+  - `COPY:file_to_copy new_location`
+  - `DELETE:file_or_dir_to_delete`
+  - `MKDIR:directory_to_create`
   - `RENAME:old_name new_name`
   - `ZIPDIR:directory_to_zip`
-  - `ZIPFILES:target_zip_file_name files to zip separated by space...`
+  - `ZIPFILES:target_zip_file_name files to zip separated by space`
--- index.js
+++ index.js
@@ -1,48 +1,147 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const io = require('@actions/io');
+const fs = require('fs');
+require('node-zip');
 
-try {
-  const buildFileName = process.env.GITHUB_WORKSPACE+'/'+core.getInput('build-file', { required: true });
-  var fs = require('fs');
+class JXBCommand {
 
-  if (fs.existsSync(buildFileName)) {
-    var contents = fs.readFileSync(buildFileName, 'utf8');
-    var lines = contents.split("\n");
-    for (var i = 0; i <= lines.length - 1; i++) {
-      var parts = lines[i].split(":");
-      var command = parts[0];
-      var args = parts[1].split(" ");
-      switch(command) {
-        case "INCVERSION":
-          console.log("Skipping INCVERSION.");
-          break;
-        case "DELETE":
-          console.log("Delete file "+args[0]);
-          break;
-        case "MINIFY":
-          console.log("Skipping MINIFY.");
-          break;
-        case "RENAME":
-          console.log("Rename file "+args[0]+" into "+args[1]);
-          break;
-        case "ZIPDIR":
-          console.log("Zip directory "+args[0]);
-          break;
-        case "ZIPFILES":
-          console.log("Zip Files into "+args[0]);
-          break;
-        default:
-          console.log("Unknown command: "+command);
-          break;
+  constructor(commandString) {
+    var parts = commandString.trim().split(":");
+    this._command = parts[0];
+    this._args = parts[1].split(" ");
+  }
+
+  async execute() {
+    switch(this._command) {
+      case "COPY":
+        console.log(`Copying file ${this._args[0]} to ${this._args[1]}`);
+        if (fs.existsSync(this._args[0])) {
+          try {
+            await io.cp(this._args[0], this._args[1], {recursive:true, force:true});
+            //fs.copyFileSync(this._args[0], this._args[1]);
+          } catch(err) {
+            console.log(err);
+            return false;
+          }
+        }
+        else {
+          console.log(`Error: File ${this._args[0]} does not exist.`);
+          return false;
+        }
+        break;
+      case "INCVERSION":
+        console.log("Skipping INCVERSION. Versions must have been incremented before commiting.");
+        break;
+      case "DELETE":
+        console.log(`Deleting file ${this._args[0]}`);
+        if (fs.existsSync(this._args[0])) {
+          try {
+            await io.rmRF(this._args[0]);
+            //fs.unlinkSync(this._args[0]);
+          } catch(err) {
+            console.log(err);
+            return false;
+          }
+        }
+        else {
+          console.log(`File ${this._args[0]} does not exist. Continuing...`);
+        }
+        break;
+      case "MINIFY":
+        console.log("Skipping MINIFY. Files must have been minified before commiting.");
+        break;
+      case "MKDIR":
+        console.log(`Creating directory ${this._args[0]}`);
+        try {
+          await io.mkdirP(this._args[0]);
+          //fs.mkdirSync(this._args[0], { recursive: true });
+        } catch(err) {
+          console.log(err);
+          return false;
+        }
+        break;
+      case "RENAME":
+        console.log(`Renaming file ${this._args[0]} into ${this._args[1]}`);
+        try {
+          fs.renameSync(this._args[0], this._args[1]);
+        } catch(err) {
+          console.log(err);
+          return false;
+        }
+        break;
+      case "ZIPDIR":
+        console.log(`Zipping directory ${this._args[0]}`);
+        //var zip = new JSZip();
+
+        break;
+      case "ZIPFILES":
+        console.log(`Zip Files into ${this._args[0]}`);
+        break;
+      default:
+        console.log(`Unknown command: ${this._command}`);
+        break;
+    }
+    return true;
+  }
+}
+
+class JXB {
+
+  readFile(buildFile) {
+    this.buildFile = buildFile;
+    this._commands = [];
+    if (fs.existsSync(buildFile)) {
+      var contents = fs.readFileSync(buildFile, 'utf8');
+      var lines = contents.split("\n");
+      for (var i = 0; i <= lines.length - 1; i++) {
+        if (lines[i].charAt(0) == '#') continue;
+        this._commands.push(new JXBCommand(lines[i]));
       }
+      return true;
+    }
+    else {
+      return false;
     }
   }
-  else {
-    core.setFailed('Build File does not exist: '+buildFileName);
+
+  async execute() {
+    var result;
+    for (var i = 0; i <= this._commands.length - 1; i++) {
+      result = await this._commands[i].execute();
+      if (!result) return i;
+    }
+    return true;
   }
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+(async () => {
+  try {
+    console.log('Welcome to JXB.')
+    const jxb = new JXB();
+    
+    var argv = require('minimist')(process.argv.slice(2));
+    var buildFile = (typeof argv.f !== 'undefined') ? argv.f : process.env.GITHUB_WORKSPACE+'/'+core.getInput('build-file', { required: true });
+
+    if (!jxb.readFile(buildFile)) {
+      core.setFailed(`Could not read build file: ${jxb.buildFile}`);
+    }
+    else {
+      var result = await jxb.execute();
+      if (result !== true) {
+        core.setFailed(`Could not execute command: ${jxb._commands[result]._command} at line ${result}`);
+      }
+      else {
+        console.log('Build finished successfully. Thanks for using JXB.');
+        outputVersion = '1.0.0';
+        core.setOutput('output-version', outputVersion);
+      }
+    }
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
