18551b79e51404c6246a3f24fcff60a049452536
Christopher Mavros <mavrosxristoforos@gmail.com>
2023-06-21 20:27:41 +0300

    Node modules update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const io = require('@actions/io');
 const fs = require('fs');
 const path = require('path');
